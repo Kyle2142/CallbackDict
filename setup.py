aa1694fa82ddd46504accea69687988e1c3376c7
Kyle2142 <19349418+Kyle2142@users.noreply.github.com>
2019-06-30 16:44:49 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="CallbackDict",  # Required
+    version="1",  # Required
+    description="A simple dict extension that uses a (possibly async) callback to fetch missing items",  # Required
+    author="https://t.me/Kyle_S",  # Optional
+    py_modules=["CallbackDict"]
+)
